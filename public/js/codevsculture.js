b59363e767dfa19b9d40739af1c026b5a7900aab
Noah Appleby <appleby.n@husky.neu.edu>
2016-10-09 09:44:17 -0400

    fixed 2x answer bug

--- public/js/codevsculture.js
+++ public/js/codevsculture.js
@@ -17,16 +17,19 @@ document.addEventListener('DOMContentLoaded', function () {
         //(cardClass == "cu" && e.throwDirection == 1) ? e.target.style.borderColor = "green": (cardClass = "co" && e.throwDirection != 1) ? e.target.style.borderColor = "green":  e.target.style.borderColor = "red";
         var cardClass = e.target.className;
 
-        if((cardClass == "cu") && (e.throwDirection == 1)) {
+        //console.log("this one" + e.target.className + " less " + cardClass.substring(0,2));
+
+        if((cardClass.substring(0,2) == "cu") && (e.throwDirection == 1)) {
           e.target.style.borderColor = "green";
-          console.log("1");
-        } else if((cardClass == "co") && (e.throwDirection == -1)) {
+          //console.log("1");
+        }
+        else if((cardClass.substring(0,2) == "co") && (e.throwDirection == -1)) {
             e.target.style.borderColor = "green";
-            console.log("2 " + cardClass);
+            //console.log("2 " + cardClass);
           }
           else {
           e.target.style.borderColor = "red";
-          console.log("3 " + cardClass);
+          //console.log("3 " + cardClass);
         }
 
         // e.target.innerHTML = "<div style=\"background-color:green\">" + e.target.innerHTML + "</div>"
